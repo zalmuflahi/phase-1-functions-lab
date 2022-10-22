91cc28f4ba32fbd55a6a97a20da2a3247f5684b9
Zaid Almuflahi <zaid.almuflahi1217@gmail.com>
2022-10-22 09:36:43 -0400

    commit tree cffb2c629babfcc8ffa9604003a8519dbc9e8246

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+// HQ = 42
+// BLOCK = 264
+function distanceFromHqInBlocks(num){
+    return Math.abs(num - 42)
+}
+function distanceFromHqInFeet(num){
+    return distanceFromHqInBlocks(num) * 264
+}
+function distanceTravelledInFeet(start, end){
+    return Math.abs(end-start)*264
+}  
+function calculatesFarePrice(start, end){
+    let feet = distanceTravelledInFeet(start, end);
+    if(feet <= 400){
+        return 0
+    } else if(feet >= 2500){
+        return 'cannot travel that far'
+    } else if(feet >= 2000){
+       return 25
+    }    
+    else{
+        let price = (feet - 400) * 0.02
+        return price 
+    }
+}
